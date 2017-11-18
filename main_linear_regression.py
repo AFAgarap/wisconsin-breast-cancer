97e0fa01a7cb6bb7c940ed8b5fbb2c22a5dee5bd
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-11-18 17:05:45 +0800

    Changes the number of epochs

--- main_linear_regression.py
+++ main_linear_regression.py
@@ -66,7 +66,7 @@ def main():
     model = LinearRegression(alpha=LEARNING_RATE, batch_size=BATCH_SIZE, num_classes=NUM_CLASSES,
                              sequence_length=num_features)
 
-    model.train(epochs=2500, log_path='./log_path/linear_regression/', train_data=[train_features, train_labels],
+    model.train(epochs=3000, log_path='./log_path/linear_regression/', train_data=[train_features, train_labels],
                 train_size=train_size, validation_data=[test_features, test_labels], validation_size=test_size,
                 result_path='./results/linear_regression/')
 
