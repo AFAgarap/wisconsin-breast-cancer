6d575ec44a770d67c20207843dd6dfa7083ed961
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-12-26 23:46:25 +0800

    feat : add CV for MLP

--- grid_search.py
+++ grid_search.py
@@ -24,8 +24,22 @@ from sklearn import datasets
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+
+
+def validate_model(model, parameter_set, train_data, test_data):
+    clf = GridSearchCV(estimator=model, param_grid=parameter_set, n_jobs=3, cv=10)
+
+    clf.fit(train_data[0], train_data[1])
+
+    grid_scores = clf.grid_scores_
+    best_score = clf.best_score_
+    best_params = clf.best_params_
+    test_score = clf.score(test_data[0], test_data[1])
+    
+    return grid_scores, best_score, best_params, test_score
 
 
 def main():
@@ -33,20 +47,33 @@ def main():
     features = dataset.data
     labels = dataset.target
     
-    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3, stratify=labels)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3,
+        stratify=labels)
     
     parameter_set = {'loss': ('hinge', 'squared_hinge'), 'C': [1, 10, 100, 1000, 5, 50, 500, 5000]}
 
     model = LinearSVC()
-    
-    clf = GridSearchCV(estimator=model, param_grid=parameter_set, n_jobs=3, cv=10)
+    grid_scores, best_score, best_params, test_score = validate_model(model=model, parameter_set=parameter_set,
+        train_data=[train_features, train_labels], test_data=[test_features, test_labels])
 
-    clf.fit(train_features, train_labels)
+    print(grid_scores)
+    print('SVM best score: {}'.format(best_score))
+    print('SVM best params : {}'.format(best_params))
+    print('SVM test score : {}'.format(test_score))
 
-    print(clf.grid_scores_)
-    print('clf.best_score_ : {}'.format(clf.best_score_))
-    print('clf.best_params_ : {}'.format(clf.best_params_))
-    print('clf.score : {}'.format(clf.score(test_features, test_labels)))
+    parameter_set = {'activation': ['identity', 'logistic', 'tanh', 'relu'],
+    'solver': ['sgd', 'adam'],
+    'batch_size': [16, 32, 64, 128],}
+
+    model = MLPClassifier()
+
+    grid_scores, best_score, best_params, test_score = validate_model(model=model, parameter_set=parameter_set,
+        train_data=[train_features, train_labels], test_data=[test_features, test_labels])
+
+    print(grid_scores)
+    print('MLP best score: {}'.format(best_score))
+    print('MLP best params : {}'.format(best_params))
+    print('MLP test score : {}'.format(test_score))
 
 
 if __name__ == '__main__':
